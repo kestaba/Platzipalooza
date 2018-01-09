d4121dbd603738ca7c916662b53f841abe2f70d9
kestaba <kestaba.dev@gmail.com>
2018-01-09 08:35:14 -0400

    Menú offcanvas y Drilldown.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 $(document).foundation()
+
+$('[data-toggle-dia]').click(function (ev) { // () => es lo mismo que function () o function nombre() para utilizar el nuevo estándar de js (arrow functions).
+    
+    var panel = $(this).data('toggleDia')// 1. const en lugar de var para utilizar el nuevo estándar de ECMAScript.
+                                          // 2. Para acceder al atributo se coloca utilizando CamelCase. No hace falta el data- pues está implicito para todos los atributos data.    
+    
+    $('#lineup-tabs').foundation('selectTab', panel)
+})
+
+const $offCanvas = $('#offCanvas')
+const $sticky = $('#sticky')
+
+$offCanvas.find('li').click(function (ev){
+    $offCanvas.foundation('close')
+    setTimeout(function(){
+        $sticky.css('left', '0px')        
+    }, 300)
+})
